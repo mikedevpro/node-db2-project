34549ce56c1d549796466bcc7e1ae42d3771135d
Michael Nobles <Mnobles33@gmail.com>
2025-04-04 19:24:53 -0700

    first commit

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,28 @@
+const knex = require('knex')
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './data/dealer.db3',
+  },
+  useNullAsDefault: true,
+});
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars');
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  return db('cars').where('id', id).first;
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = (car) => {
+  const [id] = ('cars').insert(car);
+  return getById(id);
 }
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', (table) => {
+    table.increments('id').primary();
+    table.string('vin').notNullable().unique();
+    table.string('make').notNullable();
+    table.string('model').notNullable();
+    table.integer('mileage').notNullable();
+    table.string('title').notNullable();
+    table.string('transmission').notNullable();
+  });
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars');
 };
