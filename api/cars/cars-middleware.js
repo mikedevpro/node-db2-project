e6aa16df132472ab3970f44c2d8714d399dd006c
Michael Nobles <Mnobles33@gmail.com>
2025-04-04 20:00:58 -0700

    next commit

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,6 +1,15 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const Car = require('./cars-model')
+const db = require('../../data/db-config')
+
+exports.checkCarId = (req, res, next) => {
+    if (!req.body.CarId) {
+      throw {
+        status: 404,
+        message: 'car with id {req.body.CarId} not found',
+      }
+    }
+  }
+
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars = [
+  {
+    vin: '1111111111',
+    make: 'toyota',
+    model: 'corolla',
+    mileage: 215000,
+    title: 'clean',
+    transmission: 'automatic',
+  },
+  {
+    vin: '1111111111',
+    make: 'toyota',
+    model: 'camry',
+    mileage: 150000,
+    title: 'salvage',
+  },
+  {
+    vin: '1111111111',
+    make: 'toyota',
+    model: 'prius',
+    mileage: 100000,
+  },
+]
+
+// exports.seed = function(knex) {
+//   return knex('cars')
+//   .truncate().then(() => {
+//     return knex('cars').insert(cars)
+//   })
+// }
+
+exports.seed = async function (knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}
