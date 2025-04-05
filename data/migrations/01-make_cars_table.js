aeabfe86c739823329d439d83de321bd3a69fed8
Michael Nobles <Mnobles33@gmail.com>
2025-04-04 20:08:55 -0700

    next commit

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,15 +1,15 @@
-exports.up = async function (knex) {
-  await knex.schema.createTable('cars', (table) => {
+exports.up = function (knex) {
+  return knex.schema.createTable('cars', (table) => {
     table.increments('id').primary();
-    table.string('vin').notNullable().unique();
-    table.string('make').notNullable();
-    table.string('model').notNullable();
-    table.integer('mileage').notNullable();
-    table.string('title').notNullable();
-    table.string('transmission').notNullable();
+    table.string('vin', 13).notNullable().unique();
+    table.string('make', 128).notNullable();
+    table.string('model', 100).notNullable();
+    table.integer('mileage').unsigned().notNullable();
+    table.string('title', 100)
+    table.string('transmission', 100)
   });
 };
 
-exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists('cars');
+exports.down = function (knex) {
+  knex.schema.dropTableIfExists('cars');
 };
